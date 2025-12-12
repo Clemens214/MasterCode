46d02abdf30bfce566c11557a98ff513d71853be
Uni <Clemens.Hochstaedter@physik.uni-regensburg.de>
2025-12-12 18:07:59 +0100

    finished makeSystemSI

---
+++ makeSystemSI.m
@@ -0,0 +1,224 @@
+function [totalSystem, gammaL, gammaR, varargout] = makeSystemSI (Energy, sample, eigenenergy, hoppingLead, hoppingsInter, options)
+arguments
+    Energy
+    sample
+    eigenenergy
+    hoppingLead
+    hoppingsInter
+    options.size = 1
+    options.check = true
+    options.checkMore = false
+end
+    sizeSample = length(sample);
+    sizeExtra = options.size;
+    
+    % compute the self-energies of the leads
+    sigmaL = makeSigma (sizeSample, sizeExtra, Energy, hoppingLead, hoppingsInter, eigenenergy, left=true);
+    sigmaR = makeSigma (sizeSample, sizeExtra, Energy, hoppingLead, hoppingsInter, eigenenergy, right=true);
+
+    % generate the Hamiltonian of the total system
+    totalHamiltonian = makeSystem(sample, sizeExtra, eigenenergy, hoppingsInter, hoppingLead);
+    totalSystem = totalHamiltonian + sigmaL + sigmaR;
+    
+    % compute the coupling strengths of the leads
+    gammaL = -1j*(sigmaL - sigmaL');
+    gammaR = -1j*(sigmaR - sigmaR');
+    
+    % check the results
+    if options.check == true
+        checkGamma(gammaL, 'gammaL')
+        checkGamma(gammaR, 'gammaR')
+        checkHamiltonian(totalSystem)
+    end
+    
+    varargout{1} = sigmaL;
+    varargout{2} = sigmaR;
+end
+
+%% functions used in generating the total Hamiltonian
+function [totalSystem] = makeSystem (sample, sizeExtra, eigenenergy, hoppingsInter, hoppingLead)
+arguments
+    sample
+    sizeExtra
+    eigenenergy
+    hoppingsInter
+    hoppingLead
+end
+    sizeSample = length(sample);
+    sizeTotal = sizeSample + 2*sizeExtra;
+    totalSystem = zeros(sizeTotal, sizeTotal);
+    % generate the sections
+    top = 1 : sizeExtra;
+    mid = sizeExtra+1 : sizeTotal-sizeExtra;
+    bottom = sizeTotal-sizeExtra+1 : sizeTotal;
+    % include the sample
+    totalSystem(mid, mid) = sample;
+    % include the environment
+    if sizeExtra > 0
+        % include the leads
+        totalSystem(top, top) = makeLead(eigenenergy, hoppingLead, sizeExtra, left=true);
+        totalSystem(bottom, bottom) = makeLead(eigenenergy, hoppingLead, sizeExtra, right=true);
+        % include the hopping matrices
+        % include the left side
+        interLeft = makeInter(sizeSample, sizeExtra, hoppingsInter, left=true);
+        totalSystem(mid, top) = interLeft;
+        totalSystem(top, mid) = interLeft.';
+        % include the right side
+        interRight = makeInter(sizeSample, sizeExtra, hoppingsInter, right=true);
+        totalSystem(bottom, mid) = interRight;
+        totalSystem(mid, bottom) = interRight.';
+    end
+end
+
+function [lead] = makeLead(eigenenergy, hopping, size, options)
+arguments
+    eigenenergy
+    hopping
+    size
+    options.left = false
+    options.right = false
+end
+    lead = zeros(size, size);
+    for row = 1 : size
+        for column = 1 : size
+            if row == column
+                lead(row, column) = eigenenergy;
+            elseif abs(row-column) <= 1 && options.left == true && options.right == false
+                lead(row, column) = hopping;
+            end
+        end
+    end
+end
+
+function [inter] = makeInter(sizeSample, sizeExtra, hoppingsInter, options)
+% generate the hopping matrix between the lead and the sample
+arguments
+    sizeSample
+    sizeExtra
+    hoppingsInter
+    options.left = false
+    options.right = false
+end
+    endVal = size(hoppingsInter);
+    endVal = endVal(2);
+    if options.left == true && options.right == false
+        inter = zeros(sizeSample, sizeExtra);
+        for i = 1:endVal
+            inter(i, end) = hoppingsInter(1, i);
+        end
+    elseif options.left == false && options.right == true
+        inter = zeros(sizeExtra, sizeSample);
+        for i = 1:endVal
+            inter(1, sizeSample-endVal+i) = hoppingsInter(2, i);
+        end
+    end
+end
+
+%% functions used to calculate the self- energies
+function [SigmaTotal, varargout] = makeSigma (sizeSample, sizeExtra, Energy, hoppingLead, hoppingsInter, eigenenergy, options)
+arguments
+    sizeSample
+    sizeExtra
+    Energy
+    hoppingLead
+    hoppingsInter
+    eigenenergy
+    options.left = false
+    options.right = false
+end
+    sizeTotal = sizeSample + 2*sizeExtra;
+    % return the self-energy matrix
+    [Sigma, GreensFunc] = CalcSigma(Energy, sizeExtra, hoppingLead, hoppingsInter, eigenenergy, left=options.left, right=options.right);
+    % place the self- energy matrix
+    sizeSigma = length(Sigma);
+    if options.left == true
+        range = 1 : sizeSigma;
+    elseif options.right == true
+        range = sizeTotal-sizeSigma+1 : sizeTotal;
+    end
+    SigmaTotal = zeros(sizeTotal);
+    SigmaTotal(range, range) = Sigma;
+    varargout{1} = GreensFunc;
+end
+
+function [Sigma, varargout] = CalcSigma (Energy, sizeExtra, hoppingLead, hoppingsInter, eigenenergy, options)
+arguments
+    Energy
+    sizeExtra
+    hoppingLead
+    hoppingsInter
+    eigenenergy
+    options.left = false
+    options.right = false
+end
+    % calculate the self- energy
+    if sizeExtra >= 1
+        hoppings = hoppingLead;
+    elseif sizeExtra == 0 && options.left == true
+        hoppings = hoppingsInter(1, :);
+    elseif sizeExtra == 0 && options.right == true
+        hoppings = hoppingsInter(2, :);
+    end
+    Sigma = zeros(length(hoppings), length(hoppings));
+    GreensFunc = CalcGreens(Energy, hoppingLead, eigenenergy);
+    for i = 1:length(hoppings)
+        for j = 1:length(hoppings)
+            Sigma(i, j) = hoppings(i) * GreensFunc * hoppings(j);
+        end
+    end
+    varargout{1} = GreensFunc;
+end
+
+function [G_plus] = CalcGreens (w, t, eig, options)
+arguments
+    w
+    t
+    eig
+    options.eta = 1E-12
+end
+    eta = options.eta;
+    % calculate parts of the Greens function
+    val = w - eig + 1j*eta;
+    factor = val/(2*t^2);
+    root = sqrt(1-( 4*t^2/val^2 ));
+    % calculate the final Greens function elements
+    G_plus = factor*(1 + root);
+    G_minus = factor*(1 - root);
+end
+
+%% checking functions
+function [] = checkGamma(gamma, name)
+    [~, flag] = chol(gamma);
+    if flag == 0
+        disp([name, ' is symmetric positive definite. Flag = ', num2str(flag)])
+    else
+        disp([name, ' is not symmetric positive definite. Flag = ', num2str(flag)])
+    end
+end
+
+function [] = checkHamiltonian(totalSystem)
+    Diff = totalSystem - totalSystem';
+    Diag = true;
+    offDiag = true;
+    for i = 1:length(Diff)
+        for j = 1:length(Diff)
+            if Diff(i,j) ~= 0 %not equal to zero
+                if i==j
+                    Diag = false;
+                else
+                    offDiag = false;
+                end
+            end
+        end
+    end
+    % return the result
+    if Diag == true && offDiag == true
+        disp('The Hamiltonian is totally hermitian.')
+    elseif Diag == true
+        disp('The diagonal elements of the Hamiltonian are hermitian.')
+    elseif offDiag == true
+        disp('The offdiagonal elements of the Hamiltonian are hermitian.')
+    else
+        disp('No part of the Hamiltonian is hermitian.')
+    end
+end
